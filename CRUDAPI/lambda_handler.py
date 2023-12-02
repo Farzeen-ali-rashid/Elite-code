f6f8ce89fd3f637ba30986bf948825de25d46fa5
Rashid <farzeen.rashid@accenture.com>
2023-12-02 23:37:27 +0530

    Coding CRUD API
    Experiment

--- CRUDAPI/lambda_handler.py
+++ CRUDAPI/lambda_handler.py
@@ -24,3 +24,17 @@ def lambda_handler (event, context)
     logger.info(event)                 # Log request event to see how request looks like
     httpMethod = event['httpMethod']   # extract http method from event object
     path = event['path']               # extract path from event object
+
+    if httpMethod == getMethod and path == healthPath:
+        response = buildresponse(200)
+
+def buildresponse(statuscode, body=None):
+    response = {
+        'statuscode' : statuscode,
+        'headers' :{
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : '*'      # This is important if we need to integrate with FE with differenet hostname. Allows cross region access
+        }
+    }
+
+    
